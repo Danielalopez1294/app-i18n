4624eb8e48f193a16dbf46760c39a00c609221a4
Danielalopez1294 <lopezm.daniela.6120@gmail.com>
2026-07-03 12:47:46 -0600

    Agrega archivo README con la documentación del proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonItem, IonSelect, IonSelectOption, IonButton 
+} from '@ionic/angular/standalone';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonItem, IonSelect, IonSelectOption, IonButton, 
+    TranslateModule
+  ],
 })
 export class HomePage {
-  constructor() {}
+  
+  // 1. Declaración de variables
+  idiomas = [
+    { code: 'es', label: 'Español' },
+    { code: 'en', label: 'English' }
+  ];
+
+  idiomaActual = 'es';
+
+  // 2. Constructor único con inyección de dependencias
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.idiomaActual);
+    this.translate.use(this.idiomaActual);
+  }
+
+  // 3. Métodos funcionales
+  cambiarIdioma(event: any) {
+    const idiomaSeleccionado = event.detail.value;
+    this.translate.use(idiomaSeleccionado);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
+// ngx-translate imports
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Rutas corregidas apuntando a la carpeta app/
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// Factory function para el cargador HTTP de traducciones
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(), // Necesario para cargar los JSON
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+        }
+      })
+    )
   ],
 });
